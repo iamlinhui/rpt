6b10159c0d0ce15544c82e109e61bf64a47d0342
lynn <lynn@promptness.cn>
2022-05-30 14:31:35 +0800

    stop

--- rpt-client/src/main/java/cn/promptness/rpt/client/ClientApplication.java
+++ rpt-client/src/main/java/cn/promptness/rpt/client/ClientApplication.java
@@ -95,6 +95,11 @@ public class ClientApplication implements Application<Boolean> {
     }
 
     @Override
+    public void stop() {
+        clientWorkerGroup.shutdownGracefully();
+    }
+
+    @Override
     public Bootstrap bootstrap() {
         return bootstrap;
     }
